ae8d6a60b48ee7d14d6eccc1d3e718b97bcb2d0f
Ben Baumer <ben.baumer@gmail.com>
2025-03-21 11:56:36 -0400

    exclude noreply

--- R/summarize.R
+++ R/summarize.R
@@ -10,7 +10,7 @@
 #' 
 summarize_commits <- function(...) {
   tbl_commits(...) |>
-    dplyr::filter(who != "noreply@github.com") |>
+    dplyr::filter(!grepl("noreply", x = who)) |>
     dplyr::group_by(who) |>
     dplyr::summarize(
       num_commits = dplyr::n(),
